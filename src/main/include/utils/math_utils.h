58e5c62c75265b9308c158912928059b6584bc08
TedChueh <cch7d5.ee13@nycu.edu.tw>
2026-02-22 01:28:10 +0800

    Add angular velocity header and update type alias in math_utils.h

--- src/main/include/utils/math_utils.h
+++ src/main/include/utils/math_utils.h
@@ -6,9 +6,13 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <cmath>
+#include "units/angular_velocity.h"
+
 using namespace frc;
+using TPS = units::turns_per_second_t;
 
 Rotation2d getAngleFromRobotToTarget(Translation2d target, Translation2d reference, Rotation2d direction);
 double getDistanceFromRobotToTarget(Translation2d target, Translation2d reference);
+// TPS getTPSFromDistance(double distance, double maxSpeed, double slope, double y_intercept);
 
 #endif 
